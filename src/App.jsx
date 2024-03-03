91f8d48dcee5f296d3cc364c4cf46fe731319347
Nu4ego <144213766+Nu4ego@users.noreply.github.com>
2024-03-03 03:30:24 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,132 @@
+import './App.css';
+
+//  Практические задачи
+// function App() {
+//   return (
+//     <div>
+//       <h1>text</h1>
+//       <p>абзац</p>
+//       <p>абзац</p>
+//       <input type="text" placeholder="инпут" />
+//       <br />
+//       <input type="text" placeholder="инпут" />
+//       <ul>
+//         <li> 1</li>
+//         <li> 2</li>
+//         <li> 3</li>
+//         <li> 4</li>
+//         <li> 5</li>
+//         <li> 6</li>
+//         <li> 7</li>
+//         <li> 8</li>
+//         <li> 9</li>
+//         <li> 10</li>
+//       </ul>
+//       <table>
+//         <tr>
+//           <td> таблица 1</td>
+//           <td> таблица 2</td>
+//           <td> таблица 3</td>
+//         </tr>
+//         <tr>
+//           <td> таблица 4</td>
+//           <td> таблица 5</td>
+//           <td> таблица 6</td>
+//         </tr>
+//         <tr>
+//           <td> таблица 7</td>
+//           <td> таблица 8</td>
+//           <td> таблица 9</td>
+//         </tr>
+//       </table>
+//       <p className="first-paragraph">абзац</p>
+//       <p className="second-paragraph">абзац</p>
+//       <p className="third-paragraph">абзац</p>
+//     </div>
+//   );
+// }
+
+//  Исправление ошибок
+// function App() {
+//   return (
+//     <ul>
+//       <li>text1</li>
+//       <li>text2</li>
+//       <li>text3</li>
+//     </ul>
+//   );
+// }
+
+// function App() {
+//   return (
+//     <div>
+//     <li>text2</li>
+//   <ul>
+//         <li>text1</li>
+//         <li>text2</li>
+//         <li>text3</li>
+//       </ul>
+//       <ul>
+//         <li>text1</li>
+//         <li>text2</li>
+//         <li>text3</li>
+//       </ul>
+//     </div>
+//   );
+// }
+
+// function App() {
+//   return (
+//     <input />
+//   );
+// }
+
+// function App() {
+//   return (
+//     <div>
+//       <input />
+//       <input />
+//       <input />
+//     </div>
+//   );
+// }
+
+//  Практически задачи
+// function App() {
+//   const isAdult = true;
+
+//   return (
+//     <div>
+//       {isAdult ? (
+//         <p>18+</p>
+//       ) : (
+//         <p>18-</p>
+//       )}
+//     </div>
+//   );
+// }
+
+// function App() {
+//   const isAdmin = true;
+
+//   return (
+//     isAdmin ? (
+//       <div>
+//         <p>Admin</p>
+//       </div>
+//     ) : null
+//   );
+// }
+
+//  Решение задач
+// function App() {
+//   return (
+//     <div className="green-border">
+//       Текст
+//     </div>
+//   );
+// }
+
+
+
+export default App;
